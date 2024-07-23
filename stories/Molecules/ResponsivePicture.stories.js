bf26ec766ca0a310b9a8d2ba37bc4686643c55e2
Arnaud Lanoy <arnaud.devui@gmail.com>
2024-07-23 12:21:40 +0200

    - Update import of ResponsivePicture story images

--- stories/Molecules/ResponsivePicture.stories.js
+++ stories/Molecules/ResponsivePicture.stories.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import {ResponsivePicture} from '../../overrides/app/components/responsive-picture'
 import {Box} from '@salesforce/retail-react-app/app/components/shared/ui'
+import herosm from '../../overrides/app/static/img/hero-sm.jpg'
+import heromd from '../../overrides/app/static/img/hero-md.jpg'
+import herolg from '../../overrides/app/static/img/hero-lg.jpg'
+import herosm2x from '../../overrides/app/static/img/hero-sm2x.jpg'
+import heromd2x from '../../overrides/app/static/img/hero-md2x.jpg'
+import herolg2x from '../../overrides/app/static/img/hero-lg2x.jpg'
 
 export default {
     title: 'Molecules/ResponsivePicture',
@@ -18,12 +24,12 @@ export default {
     tags: ['autodocs'],
     args: {
         src: {
-            sm: '/img/hero-sm.jpg',
-            md: '/img/hero-md.jpg',
-            lg: '/img/hero-lg.jpg',
-            sm2x: '/img/hero-sm2x.jpg',
-            md2x: '/img/hero-md2x.jpg',
-            lg2x: '/img/hero-lg2x.jpg'
+            sm: herosm,
+            md: heromd,
+            lg: herolg,
+            sm2x: herosm2x,
+            md2x: heromd2x,
+            lg2x: herolg2x
         },
         loading: 'auto',
         fetchpriority: 'high',
